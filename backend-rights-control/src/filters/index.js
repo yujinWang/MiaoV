54272f4e316286f8c7787f06e9f65a05917f0a80
yujinWang <1219044051@qq.com>
2020-05-08 11:29:20 +0800

    filter changed

--- backend-rights-control/src/filters/index.js
+++ backend-rights-control/src/filters/index.js
@@ -1,14 +1,27 @@
 // 自定义过滤器
-import Vue from 'vue'
-Vue.filter('capitalizeGlobal', (value) => {
-  // console.log(value)
+// import Vue from 'vue'
+// Vue.filter('capitalizeGlobal', (value) => {
+//   // console.log(value)
+//   if (!value) return ''
+//   return value.toUpperCase()
+// })
+// Vue.filter('ellipsisGlobal', (value, len) => {
+//   if (!value) return ''
+//   if (value.length > len) {
+//     return value.slice(0, len) + '...'
+//   }
+//   return value
+// })
+
+// 修改为函数导出方式再在main.js中处理
+export function capitalizeGlobal(value) {
   if (!value) return ''
   return value.toUpperCase()
-})
-Vue.filter('ellipsisGlobal', (value, len) => {
+}
+export function ellipsisGlobal(value, len) {
   if (!value) return ''
   if (value.length > len) {
     return value.slice(0, len) + '...'
   }
   return value
-})
+}
--- backend-rights-control/src/main.js
+++ backend-rights-control/src/main.js
@@ -9,7 +9,19 @@ import './utils/http.js'
 import './utils/dateformat.js'
 import './utils/permission.js'
 import './directives/index.js'
-import './filters/index.js'
+// import * as directives from './directives/index.js'
+// console.log(directives)
+
+// import './filters/index.js'
+import * as filters from './filters/index.js'
+// filters为引入的js文件中的所有内容
+// console.log(filters)
+// console.log(Object.keys(filters))
+// 过滤器全局注册
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 Vue.config.productionTip = false
 
 new Vue({
